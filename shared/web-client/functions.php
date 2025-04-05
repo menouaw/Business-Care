dfb1e20f568fb5e670068f970b2478230c0581ef
lucie <manarinlucie@gmail.com>
2025-04-05 21:47:12 +0200

    fix: nom de variable

--- shared/web-client/functions.php
+++ shared/web-client/functions.php
@@ -317,7 +317,7 @@ function renderPagination($pagination, $urlPattern)
  * @param int $perPage Nombre d'éléments par page, par défaut défini par la constante DEFAUT_ITEMS_PER_PAGE.
  * @return array Tableau contenant les prestations paginées ainsi que les informations de pagination.
  */
-function getPrestations($type = '', $categorie = '', $page = 1, $perPage = DEFAUT_ITEMS_PER_PAGE)
+function getPrestations($type = '', $categorie = '', $page = 1, $perPage = DEFAULT_ITEMS_PER_PAGE)
 {
     $where = [];
     $params = [];
