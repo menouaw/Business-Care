0426b24504230849b6e11f9532ca765bbdb45bf3
menouaw <menoua.khttn@gmail.com>
2025-05-01 14:17:06 +0200

    refactor: tâche programmée de génération du rapport

--- java-app/docker-entrypoint.sh
+++ java-app/docker-entrypoint.sh
@@ -4,12 +4,5 @@ set -e
 echo "Génération du rapport initial..."
 java -jar /app/app.jar
 
-echo "Démarrage du planificateur pour exécuter le rapport quotidiennement à 2:00 AM..."
-while true; do
-    current_time=$(date +%H:%M)
-    if [ "$current_time" = "02:00" ]; then
-        echo "Exécution du rapport quotidien à $(date)"
-        java -jar /app/app.jar
-    fi
-    sleep 30
-done 
+echo "Démarrage du service cron actif..."
+crond -f
